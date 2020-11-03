76ec31a75d69f02101822eca32c2fcadf63cb979
zhaozh <zzh@aidipper.com>
2020-11-03 10:55:02 +0800

    mono运行时测试

---
+++ 02-mono/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            //编译 docker build -t mono-consoleapp .
+            Console.WriteLine("Hello Mono World");
+        }
+    }
+}
